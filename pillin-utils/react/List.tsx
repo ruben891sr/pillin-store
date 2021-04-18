88f5c9f77d5c3e4ff0bfe1f416071ba47989ec0f
BruceSRC <ivan.barrera@sr-consultores.com>
2021-04-18 16:33:14 -0400

    444

--- pillin-utils/react/List.tsx
+++ pillin-utils/react/List.tsx
@@ -1,22 +1,40 @@
-//react/StoreList.tsx
-import React from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from 'react'
+import { injectIntl, FormattedMessage } from 'react-intl'
+import { graphql, compose, useLazyQuery } from 'react-apollo'
+import { Spinner } from 'vtex.styleguide'
+import { useCssHandles } from 'vtex.css-handles'
 
-interface StoreListProps {}
+const CSS_HANDLES = [
+  'container',
+  'storesListCol',
+  'storesList',
+  'storesMapCol',
+  'noResults',
+  'listingMapContainer',
+  'loadAll',
+] as const
 
-const StoreList: StorefrontFunctionComponent<StoreListProps> = ({}) => 
-{
+const StoreList = ({
+  orderForm: { called: ofCalled, loading: ofLoading, orderForm: ofData },
+  googleMapsKeys,
+  filterByTag,
+  icon,
+  iconWidth,
+  iconHeight,
+}) => {
+
+
    return (
      <div>
-       <h1> Testy test</h1>
+       <h1>Countdown Test</h1>
      </div>
    )
 }
 
-StoreList.schema = {
-  title: 'editor.storelist.title',
-  description: 'editor.storelist.description',
-  type: 'object',
-  properties: {},
-}
+export default injectIntl()
 
-export default StoreList
+  )(StoreList)
+)
