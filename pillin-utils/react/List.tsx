f58765c83561018ed37fdbd3f241ff630bbd2895
BruceSRC <ivan.barrera@sr-consultores.com>
2021-04-18 16:42:38 -0400

    iii

--- pillin-utils/react/List.tsx
+++ pillin-utils/react/List.tsx
@@ -4,14 +4,9 @@
 import React from 'react'
 //import React, { useState } from 'react'
 //import { injectIntl, FormattedMessage } from 'react-intl'
-import { injectIntl } from 'react-intl'
-
-import { graphql, compose } from 'react-apollo'
 //import { Spinner } from 'vtex.styleguide'
 //import { useCssHandles } from 'vtex.css-handles'
 
-import ORDER_FORM from './queries/orderForm.gql'
-import GOOGLE_KEYS from './queries/GetGoogleMapsKey.graphql'
 
 /*
 const CSS_HANDLES = [
@@ -34,19 +29,5 @@ const StoreList = ({ }) => {
    )
 }
 
-export default injectIntl(
-  compose(
-    graphql(ORDER_FORM, {
-      name: 'orderForm',
-      options: {
-        ssr: false,
-      },
-    }),
-    graphql(GOOGLE_KEYS, {
-      name: 'googleMapsKeys',
-      options: {
-        ssr: false,
-      },
-    })
-  )(StoreList)
-)
+export default StoreList
+
