766dcaa75f1650e18bcbc220a71af26afd643a57
BruceSRC <ivan.barrera@sr-consultores.com>
2021-04-18 17:59:26 -0400

    asisi

--- pillin-utils/react/List.tsx
+++ pillin-utils/react/List.tsx
@@ -59,8 +59,6 @@ const StoreList = ({ }) => {
 
 export default StoreList
 
-import { PixelMessage } from './typings/events'
-
 export function handleEvents(e: PixelMessage) {
   switch (e.data.eventName) {
     case 'vtex:pageView': {
