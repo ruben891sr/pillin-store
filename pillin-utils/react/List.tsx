e812659f7196ae1811a0fd670a38200f1f728c0a
Mario Barrera <mario.barrera@sr-consultores.com>
2021-05-06 16:56:07 -0400

    Test.

--- pillin-utils/react/List.tsx
+++ pillin-utils/react/List.tsx
@@ -15,11 +15,77 @@ const StoreList = ({ }) => {
           <title>parsedTitle</title>
           <meta name="description" content="parsedDescription" />
           <script src="/files/pillin-custom.js"></script>
+		  <style>
+            .gm-style-iw button {
+                display: none !important;
+            }
+            * {
+                box-sizing: border-box;
+            }
+            #cont {
+                position: relative;
+                overflow: hidden;
+                min-height: 30em;
+            }
+            #form {
+                text-align: left;
+                vertical-align: top;
+
+                position: relative;
+                z-index: 1;
+                padding: 2em;
+                background-color: #F7F1E7;
+                box-shadow: 0 0 1em -0.2em black;
+            }
+            #map {
+                position: relative;
+                height: 20em;
+            }
+            #map > * { 
+                position: absolute;
+                top: 0;
+                right: 0;
+                bottom: 0;
+                left: 0;
+            }
+            #form > * { 
+                margin: 0;
+                padding: 0.5em;
+            }
+            #form > label {
+                display: block;
+            }
+            #form > select {
+                min-width: 100%;
+            }
+            #store_info {
+                visibility: collapse;
+            }
+            #store_info.populated {
+                visibility: visible;
+            }
+            @media (min-width: 991px)
+            {
+                #form {
+                    margin: 4em;
+                    width: 30em;
+                    border-radius: 0.5em;
+                }
+                #map {
+                    position: absolute;
+                    height: auto;
+                    top: 0;
+                    right: 0;
+                    bottom: 0;
+                    left: 0;
+                }
+            }
+			</style>
         </Helmet>
       )}
         <div id={`cont`} className={`pillin-pillin-utils-0-x-cont srtest0`}>
             <div id={`form`} className={`pillin-pillin-utils-0-x-form`}>
-                <h1>Nuestras Tiendas 2</h1>
+                <h1>Nuestras Tiendas</h1>
                 <label>Seleccione la región</label>
                 <select id={`address_region`} title={`Seleccionar región`}>
                 </select>
